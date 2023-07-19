8563b415d2d03ba1d3e1190e2317e670a1dfaccb
Muhammad Usman Amjad <muhammadusmanamjad1@gmail.com>
2023-07-19 14:27:05 +0500

    Sidebar designing and Feed Integration with Demo Data

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import SideBar from "./SideBar";
+import Videos from "./Videos";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+	return (
+		<Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+			<Box
+				sx={{
+					height: { sx: "auto", md: "92vh" },
+					borderRight: "1px solid #3d3d3d",
+					px: { sx: 0, md: 2 },
+				}}>
+				<SideBar />
+				<Typography
+					className="copywright"
+					variant="body2"
+					sx={{ mt: 1.5, color: "#fff" }}>
+					Copyright 2022 SME
+				</Typography>
+			</Box>
+			<Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+				<Typography
+					variant="h4"
+					fontWeight="bold"
+					mb={2}
+					sx={{ color: "white" }}>
+					New
+					<span style={{ color: "#F31503" }}> Videos</span>
+				</Typography>
+				<Videos></Videos>
+			</Box>
+		</Stack>
+	);
+};
 
-export default Feed
+export default Feed;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,24 @@ import { Link } from "react-router-dom";
 import { logo } from "../utils/constants";
 import SearchBar from "./SearchBar";
 
-const Navbar = () => (
-	<Stack
-		direction="row"
-		alignItems="center"
-		p={2}
-		sx={{
-			position: "sticky",
-			background: "#000",
-			top: 0,
-			justifyContent: "space-between",
-		}}>
-		<Link to={"./"} style={{ display: "flex", alignItems: "center" }}>
-			<img src={logo} alt="Logo" height={45}></img>
-		</Link>
-		<SearchBar></SearchBar>
-	</Stack>
-);
+const Navbar = () => {
+	return (
+		<Stack
+			direction="row"
+			alignItems="center"
+			p={2}
+			sx={{
+				position: "sticky",
+				background: "#000",
+				top: 0,
+				justifyContent: "space-between",
+			}}>
+			<Link to={"./"} style={{ display: "flex", alignItems: "center" }}>
+				<img src={logo} alt="Logo" height={45}></img>
+			</Link>
+			<SearchBar></SearchBar>
+		</Stack>
+	);
+};
 
 export default Navbar;
